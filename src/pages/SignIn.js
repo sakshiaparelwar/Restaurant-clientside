776ea62bab067527b9a0a99a48fdd74aa853bb15
sakshiaparelwar <sakshispparelwar200@gmail.com>
2023-11-25 20:14:45 +0530

    changes made

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -24,6 +24,7 @@ function SignIn() {
           alert("You have Signup successfully");
           navigate("/login");
         } else {
+          console.log("something is wrong");
           Promise.reject();
         }
       })
